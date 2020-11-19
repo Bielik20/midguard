59bd82c44a2bc540a3cbdf44f00bf49a9c9836dc
Damian Bielecki <bielik20@gmail.com>
2020-11-19 11:53:17 +0100

    chore: make so that output is extendable

--- packages/core/src/lib/middleware.spec.ts
+++ packages/core/src/lib/middleware.spec.ts
@@ -28,68 +28,79 @@ describe('Middleware', () => {
     type FooInput = {
       start: string;
     };
-    type FooInputAdd = {
+    type FooInputExt = {
       foo: string;
     };
     type FooOutput = {
       end: string;
     };
-    type FooOutputMod = {
-      end: string;
+    type FooOutputExt = {
       foo: string;
     };
-    function foo(): Middleware<FooInput, FooInputAdd, FooOutput, FooOutputMod> {
+    function foo(): Middleware<FooInput, FooInputExt, FooOutput, FooOutputExt> {
       return (input, next) => {
+        console.log('foo before');
         const output = next({ foo: 'foo' });
+        console.log('foo after');
 
-        return { ...output, foo: 'foo' };
+        // return { ...output, foo: 'foo' };
+        return { foo: 'foo' };
       };
     }
 
     type BarInput = {
       start: string;
     };
-    type BarInputAdd = {
+    type BarInputExt = {
       bar: string;
     };
     type BarOutput = {
       end: string;
     };
-    type BarOutputMod = {
-      end: string;
+    type BarOutputExt = {
       bar: string;
     };
-    function bar(): Middleware<BarInput, BarInputAdd, BarOutput, BarOutputMod> {
+    function bar(): Middleware<BarInput, BarInputExt, BarOutput, BarOutputExt> {
       return (input, next) => {
+        console.log('bar before');
         const output = next({ bar: 'bar' });
+        console.log('bar after');
 
-        return { ...output, bar: 'bar' };
+        // return { ...output, bar: 'bar' };
+        return { bar: 'bar' };
       };
     }
 
-    function noop(): Middleware<unknown, unknown> {
-      return (input, next) => {
-        const output = next({});
+    // function noop(): Middleware<unknown, unknown> {
+    //   return (input, next) => {
+    //     const output = next({});
+    //
+    //     return { noop: 'noop' };
+    //   };
+    // }
 
-        return { noop: 'noop' };
-      };
-    }
-
-    const handler: MiddlewareNext<FooInputAdd, BarOutput> = (input) => {
-      console.log(input.foo);
+    const handler: MiddlewareNext<FooInputExt, BarOutput> = (input) => {
+      // console.log(input);
+      // console.log(input.foo);
+      console.log('handler');
       return { end: 'end' };
     };
 
-    const combinedFooBar = middleware(foo(), bar());
-    const combinedBarNoop = middleware(noop(), bar());
-    const fooImp = foo();
-    const barImp = bar();
-    const combinedFooBarImp = middleware(fooImp, barImp);
     const okish = middleware(foo(), bar(), handler);
-    const notOkCall = middleware(foo(), bar(), handler)({ start: 'ss' });
-    const notOk = middleware(foo(), bar(), (input) => {
-      console.log(input.foo);
-      return { end: 'end' };
-    });
+    const result = okish({ start: 'start' });
+
+    console.log('result', result);
+
+    // const combinedFooBar = middleware(foo(), bar());
+    // const combinedBarNoop = middleware(noop(), bar());
+    // const fooImp = foo();
+    // const barImp = bar();
+    // const combinedFooBarImp = middleware(fooImp, barImp);
+    // const okish = middleware(foo(), bar(), handler);
+    // const notOkCall = middleware(foo(), bar(), handler)({ start: 'ss' });
+    // const notOk = middleware(foo(), bar(), (input) => {
+    //   console.log(input.foo);
+    //   return { end: 'end' };
+    // });
   });
 });
--- packages/core/src/lib/middleware.ts
+++ packages/core/src/lib/middleware.ts
@@ -1,6 +1,6 @@
 import { noopMiddleware } from './utils';
 
-export interface Middleware<TIn, TInExt, TOut = unknown, TOutMod = TOut> {
+export interface Middleware<TIn, TInExt = void, TOut = unknown, TOutMod = TOut> {
   (input: TIn, next: MiddlewareNext<TInExt, TOut>): TOutMod;
 }
 
@@ -17,23 +17,23 @@ export function middleware<T1, T2, T3, T4>(
   fn1: Middleware<T1, T2, T3, T4>,
   fn2: MiddlewareNext<T1 & T2, T3>,
   ...fns: never[]
-): MiddlewareNext<T1, T4>;
+): MiddlewareNext<T1, T3 & T4>;
 export function middleware<T1, T2, T3, T4, T5, T6>(
-  fn1: Middleware<T1, T2, T3, T4>,
+  fn1: Middleware<T1, T2, T6 & T3, T4>,
   fn2: Middleware<T1 & T2, T5, T6, T3>,
-): Middleware<T1, T1 & T2 & T5, T6, T4>;
+): Middleware<T1, T1 & T2 & T5, T6, T6 & T3 & T4>;
 
 export function middleware<T1, T2, T3, T4, T5, T6>(
-  fn1: Middleware<T1, T2, T3, T4>,
+  fn1: Middleware<T1, T2, T6 & T3, T4>,
   fn2: Middleware<T1 & T2, T5, T6, T3>,
   fn3: MiddlewareNext<T1 & T2 & T5, T6>,
   ...fns: never[]
-): MiddlewareNext<T1, T4>;
+): MiddlewareNext<T1, T6 & T3 & T4>;
 export function middleware<T1, T2, T3, T4, T5, T6, T7, T8>(
-  fn1: Middleware<T1, T2, T3, T4>,
-  fn2: Middleware<T1 & T2, T5, T6, T3>,
+  fn1: Middleware<T1, T2, T8 & T6 & T3, T4>,
+  fn2: Middleware<T1 & T2, T5, T8 & T6, T3>,
   fn3: Middleware<T1 & T2 & T5, T7, T8, T6>,
-): Middleware<T1, T1 & T2 & T5 & T7, T8, T4>;
+): Middleware<T1, T1 & T2 & T5 & T7, T8, T8 & T6 & T3 & T4>;
 
 export function middleware(
   ...fns: (Middleware<any, any, any, any> | MiddlewareNext<any, any>)[]
@@ -49,11 +49,19 @@ function middlewareFromArray<TIn, TInMod, TOut, TOutMod>(
   }
 
   if (fns.length === 1) {
-    return fns[0]; // TODO?
+    return fns[0];
   }
 
-  return fns.reduceRight(
-    (result: Middleware<any, any, any, any>, fn: Middleware<any, any, any, any>) => (input, next) =>
-      fn(input, (nextInput) => result({ ...input, ...nextInput }, next)),
-  );
+  return fns.reduceRight((result: Middleware<any, any, any, any>, fn: Middleware<any, any, any, any>) => {
+    return (input, next) => {
+      let c = {};
+      const b = fn(input, (nextInput) => {
+        c = result({ ...input, ...nextInput }, next);
+
+        return c;
+      });
+
+      return { ...c, ...b };
+    };
+  });
 }
